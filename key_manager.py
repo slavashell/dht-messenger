4d770fa3853ee950df262fe1c265dc9a4208ce68
godlikevaslas <vmshelkovnikov@gmail.com>
2022-03-23 17:03:57 +0300

    fix

--- key_manager.py
+++ key_manager.py
@@ -4,10 +4,16 @@ import json
 from nacl.public import PrivateKey
 
 class KeyManager:
-    def __init__(self, private_key: str, public_key: str, public_keys: tp.Dict[str, str] = None) -> None:
+
+    CACHE_FILE = 'cached_keys.json'
+    PRIVATE_KEY = 'private.key'
+    PUBLIC_KEY = 'public.key'
+
+    def __init__(self, private_key: str, public_key: str, public_keys: tp.Dict[str, str] = None, cache_dir = ".") -> None:
         self._private_key = private_key
         self._public_key = public_key
         self._keys = {} if public_keys is None else public_keys
+        self._cache_dir = cache_dir
 
     @staticmethod
     def key_to_string(key: PrivateKey) -> str:
@@ -15,12 +21,12 @@ class KeyManager:
 
     @staticmethod
     def from_file(path: str) -> 'KeyManager':
-        with open(path + '/private.key', 'r') as private_key_file:
+        with open(path + '/' + KeyManager.PRIVATE_KEY, 'r') as private_key_file:
             private_key = private_key_file.readlines()
-        with open(path + '/public.key', 'r') as public_key_file:
+        with open(path + '/' + KeyManager.PUBLIC_KEY, 'r') as public_key_file:
             public_key = public_key_file.readlines()
-        with open(path + '/cached_keys.json') as cached_keys:
-            keys = json.loads(cached_keys)
+        with open(path + '/' + KeyManager.CACHE_FILE) as cached_keys:
+            keys = json.load(cached_keys)
         return KeyManager(private_key, public_key, keys)
 
     @staticmethod
@@ -47,3 +53,5 @@ class KeyManager:
 
     def add_key(self, name: str, key: str) -> None:
         self._keys[name] = key
+        with open(self._cache_dir + '/' + KeyManager.CACHE_FILE, 'w') as cache_file:
+            json.dump(self._keys, cache_file)
