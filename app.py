24e98b889e1ac4f0bb4e13ac65cf02a426c4f982
Gershon Shamailov <g.shamailov@yandex.ru>
2022-03-23 18:18:36 +0300

    implemented interface for adding new chat

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import time
 import py_cui
 import requests
 
+from typing import List
+
 
 class ClientWrapper:
     def __init__(self, host: str, port: str):
@@ -17,26 +19,37 @@ class ClientWrapper:
         response = requests.post(url=self.url + "send_message", json=data)
         response.raise_for_status()
 
-    def read_messages(self, name: str):
+    def read_messages(self, name: str) -> List[str]:
         params = {
             "name": name,
         }
         response = requests.get(url=self.url + "read_messages", params=params)
         if response.status_code != 200:
-            return None
+            return []
         return response.json()
+
+    def add_chat(self, name: str, key: str):
+        data = {
+            "name": name,
+            "key": key,
+        }
+        response = requests.post(url=self.url + "add_chat", json=data)
+        response.raise_for_status()
 
 
 class Application:
     def __init__(self, cui: py_cui.PyCUI, client: ClientWrapper):
-        self.cui = cui
-        self.client = client
-        self.nickname = "Vasya"  # TODO
+        self.cui: py_cui.PyCUI = cui
+        self.client: ClientWrapper = client
+        self.nickname: str = "Vasya"  # TODO
+        self.chats: List[str] = []
 
         self.chats_list_cell = self.cui.add_scroll_menu("Chats", 0, 0, row_span=5, column_span=1)
         self.chat_cell = self.cui.add_scroll_menu("Chat", 0, 1, 5, 5)
         self.input_cell = self.cui.add_text_box("Message:", 5, 1, 1, 5)
+        self.add_chat_cell = self.cui.add_text_box("New chat:", 7, 1, 1, 5)
         self.input_cell.add_key_command(py_cui.keys.KEY_ENTER, self.send_message)
+        self.add_chat_cell.add_key_command(py_cui.keys.KEY_ENTER, self.add_chat)
         self.cui.move_focus(self.input_cell)
         # self.chats_list_cell.set_on_selection_change_event(self.refresh)
         # self.cui.set_on_draw_update_func(self.refresh)
@@ -44,42 +57,43 @@ class Application:
 
     def send_message(self):
         message = self.input_cell.get()
-        companion = self.chats_list_cell.get()
-        if not companion:
+        if not message:
             return
 
-        self.client.send_message(companion, message)
+        name = self.chats_list_cell.get()
+        if not name:
+            return
+
+        self.client.send_message(name, message)
         self.input_cell.clear()
         self.refresh_chat()
 
     def refresh_chats_list(self):
         self.refresh_chat()
-        companions = ["Alice"]
-        if not companions:
+        if not self.chats:
             self.chats_list_cell.clear()
             return
 
         shown_chats = self.chats_list_cell.get_item_list()
-        current_chats = ["Alice"]
-        if shown_chats == current_chats:
+        if shown_chats == self.chats:
             return
 
         chat = self.chats_list_cell.get()
         self.chats_list_cell.clear()
-        for c in companions:
+        for c in self.chats:
             self.chats_list_cell.add_item(c)
         self.chats_list_cell.set_selected(chat)
 
     def refresh_chat(self):
-        companion = self.chats_list_cell.get()
-        if not companion:
+        name = self.chats_list_cell.get()
+        if not name:
             self.chat_cell.clear()
             return
 
-        self.chat_cell.set_title("Chat with {}".format(companion))
+        self.chat_cell.set_title("Chat with {}".format(name))
         shown_count = len(self.chat_cell.get_item_list())
 
-        messages = self.client.read_messages(companion)
+        messages = self.client.read_messages(name)
 
         if messages is None:
             return
@@ -91,6 +105,16 @@ class Application:
         for message in messages:
             self.chat_cell.add_item(message)
 
+    def add_chat(self):
+        name, key = self.add_chat_cell.get().split(" ", 1)
+        if not name or not key:
+            return
+
+        self.client.add_chat(name, key)  # TODO: try/except
+        self.add_chat_cell.clear()
+        self.chats.append(name)
+        self.refresh_chats_list()
+
     def start_background_updating(self):
         operation_thread = threading.Thread(target=self.refresh, daemon=True)
         operation_thread.start()
@@ -101,12 +125,12 @@ class Application:
     def refresh(self):
         while True:
             self.refresh_chats_list()
-            time.sleep(1)
+            time.sleep(5)
 
 
 def main():
     root = py_cui.PyCUI(8, 6)
-    root.set_refresh_timeout(1)
+    root.set_refresh_timeout(5)
     root.set_title("Mock")
 
     Application(root, ClientWrapper("localhost", "8000"))
